3f1fa636143eb64bd1f6b0f804128f4ffb8c0411
HYR <huangyiran@yolanda.hk>
2021-12-16 20:31:49 +0800

    update(master):适配安卓12

--- app/src/main/java/com/qingniu/qnble/demo/util/AndroidPermissionCenter.java
+++ app/src/main/java/com/qingniu/qnble/demo/util/AndroidPermissionCenter.java
@@ -3,6 +3,8 @@ package com.qingniu.qnble.demo.util;
 import android.Manifest;
 import android.app.Activity;
 import android.content.pm.PackageManager;
+import android.os.Build;
+
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
@@ -21,24 +23,52 @@ public class AndroidPermissionCenter {
             "android.permission.ACCESS_COARSE_LOCATION",
             "android.permission.ACCESS_FINE_LOCATION"};
 
+    //安卓12需要申请的权限列表
+    private static String[] ANDROID_S_PERMISSIONS_STORAGE = {
+            "android.permission.READ_EXTERNAL_STORAGE",
+            "android.permission.WRITE_EXTERNAL_STORAGE",
+            "android.permission.BLUETOOTH_SCAN",
+            "android.permission.BLUETOOTH_ADVERTISE",
+            "android.permission.BLUETOOTH_CONNECT"
+    };
+
     public static int REQUEST_EXTERNAL_STORAGE = 201;
     public static int REQUEST_CAMERA= 202;
 
     //API>=23时才需要判断权限，扫描时再判断权限
     public static void verifyPermissions(Activity activity) {
         //判断是否已经赋予权限
-        if (ContextCompat.checkSelfPermission(activity,
-                Manifest.permission.ACCESS_COARSE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
-            //如果应用之前请求过此权限但用户拒绝了请求，此方法将返回 true。
-            if (ActivityCompat.shouldShowRequestPermissionRationale(activity,
-                    Manifest.permission.ACCESS_COARSE_LOCATION)) {
-                //这里可以写个对话框之类的项向用户解释为什么要申请权限，并在对话框的确认键后续再次申请权限
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.S){
+            if (ContextCompat.checkSelfPermission(activity,
+                    Manifest.permission.ACCESS_COARSE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED) {
+                //如果应用之前请求过此权限但用户拒绝了请求，此方法将返回 true。
+                if (ActivityCompat.shouldShowRequestPermissionRationale(activity,
+                        Manifest.permission.ACCESS_COARSE_LOCATION)) {
+                    //这里可以写个对话框之类的项向用户解释为什么要申请权限，并在对话框的确认键后续再次申请权限
 
-            } else {
-                //申请权限，字符串数组内是一个或多个要申请的权限，1是申请权限结果的返回参数，在onRequestPermissionsResult可以得知申请结果
-                ActivityCompat.requestPermissions(activity,
-                        PERMISSIONS_STORAGE, REQUEST_EXTERNAL_STORAGE);
+                } else {
+                    //申请权限，字符串数组内是一个或多个要申请的权限，1是申请权限结果的返回参数，在onRequestPermissionsResult可以得知申请结果
+                    ActivityCompat.requestPermissions(activity,
+                            PERMISSIONS_STORAGE, REQUEST_EXTERNAL_STORAGE);
+                }
+            }
+        }
+        else {
+            //安卓12申请新的权限队列
+            if (ContextCompat.checkSelfPermission(activity,
+                    Manifest.permission.BLUETOOTH_SCAN)
+                    != PackageManager.PERMISSION_GRANTED) {
+                //如果应用之前请求过此权限但用户拒绝了请求，此方法将返回 true。
+                if (ActivityCompat.shouldShowRequestPermissionRationale(activity,
+                        Manifest.permission.BLUETOOTH_SCAN)) {
+                    //这里可以写个对话框之类的项向用户解释为什么要申请权限，并在对话框的确认键后续再次申请权限
+
+                } else {
+                    //申请权限，字符串数组内是一个或多个要申请的权限，1是申请权限结果的返回参数，在onRequestPermissionsResult可以得知申请结果
+                    ActivityCompat.requestPermissions(activity,
+                            ANDROID_S_PERMISSIONS_STORAGE, REQUEST_EXTERNAL_STORAGE);
+                }
             }
         }
     }
