adb5e1ca7e0b14724f423a00935567391b6084b0
HYR <huangyiran@yolanda.hk>
2022-07-05 09:27:13 +0800

    update(r-2.8.0)

---
+++ app/src/main/java/com/qingniu/qnble/demo/view/RulerActivity.java
@@ -0,0 +1,11 @@
+package com.qingniu.qnble.demo.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * @author: hyr
+ * @date: 2022/7/5 09:22
+ * @desc:
+ */
+public class RulerActivity extends AppCompatActivity {
+}
